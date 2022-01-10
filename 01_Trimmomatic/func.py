4bb816129e81f7ae22a9a8653d315ee7b2505267
junhochoi-dev <cjh970422@gmail.com>
2022-01-10 12:27:56 +0900

    log directory / trimmomatic log

--- 01_Trimmomatic/func.py
+++ 01_Trimmomatic/func.py
@@ -14,6 +14,7 @@ def trimming(sample_code):
     print("TRIMMING 실행")
     sample_code_forward = sample_code + '_1'
     sample_code_reverse = sample_code + '_2'
+    os.system('mkdir ' + urls.url_log + '/' + sample_code + '/')
     os.system(
         'java -jar /program/Trimmomatic/trimmomatic-0.39.jar PE'
         + ' ' +
@@ -31,7 +32,7 @@ def trimming(sample_code):
         + '/disk4/bicjh/rna_seq/00_data/data__samples/' + sample_code + '/' + sample_code_reverse + '_unpaired.fastq.gz' + ' '
 
         'ILLUMINACLIP:/program/Trimmomatic/adapters/TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 MINLEN:36'
-        + ' 2>' + urls.url_log + '/' + sample_code + '_log' + '$log'
+        + ' 2>' + urls.url_log + '/' + sample_code + '/' + sample_code + '_Trimmomatic_log' + '$log'
     )
     # 로그에 트리모메틱이라는 명칭 넣어주면 좋을 것 같다.
     
--- 01_Trimmomatic/trimmomatic.py
+++ 01_Trimmomatic/trimmomatic.py
@@ -3,5 +3,7 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import urls
 import func
 
+# PAIRED END만 돌아가도록 설정.
+# SINGLE END 설정 따로 해주자.
 def trimmomatic(sample_code):
     func.trimming(sample_code)
