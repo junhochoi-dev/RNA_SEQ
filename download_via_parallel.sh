4c94e3e2948448eeb1743789fa2455c7881e00c2
junhochoi-dev <cjh970422@gmail.com>
2022-01-13 13:56:22 +0900

    downloadsample fix

--- download_via_parallel.sh
+++ download_via_parallel.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-line=$1
-path=$2
+line=$1 # sample code
+path=$2 # sample folder path
+# echo $line
+# echo $path
+# echo $path$line/$line.sra
+# pwd
 prefetch -O $path -X 999999999 $line
-cd $path
-if [ -e $line.sra ]; then
-	parallel-fastq-dump -s $line.sra -t 30 -O $path --tmpdir ../ --split-files --gzip && rm $line.sra
+if [ -e $path$line/$line.sra ]; then
+	parallel-fastq-dump --sra-id $path$line/$line.sra -t 30 --outdir $path$line --tmpdir ../ --split-files --gzip # && rm $path.sra
 	# single end -> fastq-dump <SRA format>
 	# pairend end -> fastq-dump --split-files <SRA format>pre
-	#-t |--threads         number of threads
-	#-s |--sra-id          SRA id
-	#-O |--outdir          output directory
+	#-t | --threads         number of threads
+	#-s | --sra-id          SRA id
+	#-O | --outdir          output directory
 else
 	echo '[ERROR]' $line 'Apparently Not Successfully Downloaded' && exit 1
 fi
-cd ..
-rm -rf $line
+rm -rf $path$line/$line.sra
 
 # $1, $2 ... $n 셀 실행 후 입력되는 매개변수를 해당 인자로 받는다.
+
+# METADATA 도 다운로드 해야한다. *****
