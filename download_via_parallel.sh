7ed641bef44875218c294dabdc534f8a4221b86d
junhochoi-dev <cjh970422@gmail.com>
2022-01-13 11:36:21 +0900

    download sample shell fix

--- download_via_parallel.sh
+++ download_via_parallel.sh
@@ -2,14 +2,14 @@
 line=$1
 path=$2
 prefetch -O $path -X 999999999 $line
-cd $line
+cd $path
 if [ -e $line.sra ]; then
 	parallel-fastq-dump -s $line.sra -t 30 -O $path --tmpdir ../ --split-files --gzip && rm $line.sra
 	# single end -> fastq-dump <SRA format>
 	# pairend end -> fastq-dump --split-files <SRA format>pre
-	#-t|--threads         number of threads
-	#-s|--sra-id          SRA id
-	#-O|--outdir          output directory
+	#-t |--threads         number of threads
+	#-s |--sra-id          SRA id
+	#-O |--outdir          output directory
 else
 	echo '[ERROR]' $line 'Apparently Not Successfully Downloaded' && exit 1
 fi
--- func.py
+++ func.py
@@ -6,7 +6,7 @@ def download_sample(sample_code):
     print("##### START DOWNLOAD SAMPLE" + sample_code)
     ### 파일 존재여부
     os.system('mkdir ' + urls.url_samples + sample_code)
-    os.system('sh download_via_parallel.sh ' + sample_code + ' ..' + urls.url_samples + sample_code)
+    os.system('sh download_via_parallel.sh ' + sample_code + './00_data/data__samples/')
     print("##### END DOWNLOAD SAMPLE" + sample_code)
 
 def make_species_dir():
@@ -14,6 +14,6 @@ def make_species_dir():
     # os.system('pwd')
     # os.system('cd /disk4/bicjh/rna_seq/')
     for idx in range(species_data.shape[0]):
-        os.system('mkdir /disk4/bicjh/rna_seq/00_data/data__species/' + species_data.Scientific_name[idx])
+        os.system('mkdir ' + urls.url_species + species_data.Scientific_name[idx])
             
 download_sample('SRR14267547')
