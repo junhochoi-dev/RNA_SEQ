5d5a45c9d63c55f333b594ba959944f79cb3ffb9
junhochoi-dev <cjh970422@gmail.com>
2022-01-26 19:26:08 +0900

    SE trimmomatic

--- func_Trimmomatic.py
+++ func_Trimmomatic.py
@@ -6,7 +6,9 @@ import urls
 def trimming_se(sample_code, thread_value):
     print("##### START TRIMMOMATIC SE :: " + sample_code)
     
-    # java -jar trimmomatic-0.35.jar SE -phred33 input.fq.gz output.fq.gz ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+    os.system(
+        'java -jar trimmomatic-0.35.jar SE -phred33 input.fq.gz output.fq.gz ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36'
+    )
     
     print("##### END TRIMMOMATIC SE :: " + sample_code)
 
